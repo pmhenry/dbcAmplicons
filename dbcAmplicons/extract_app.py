b68d19e45457945dd783e5096a72da3f14d84038
pmhenry <pmhenry@ucdavis.edu>
2018-07-10 13:15:44 -0700

    Additional sorting and multiprocessors
    added sorting by species, order, family and phylum. Can use multi-processors, but using more than 1 makes the program run slower

--- dbcAmplicons/extract_app.py
+++ dbcAmplicons/extract_app.py
@@ -4,6 +4,45 @@ import sys, os, time, gzip
 import time
 start_time = time.time()
 from dbcAmplicons import OneReadIlluminaRun
+#from multiprocessing import Pool
+import multiprocessing
+#from itertools import product
+from functools import partial
+
+
+
+def extractFastq(vGoodReads, batch, name, batchName):
+	vContinue=False					
+	first_variable=""
+	print "tset2"
+	check_first_varable=True
+	write_list=[]
+	x=0
+	with open(batchName, "r") as file:
+		for read in (file): #loops over the file. 
+
+		 	 	######## @M may not be a good variable as it may change with speceis. save first two characters of line 1 and sort by that?
+			if check_first_varable==True: #this should fix problem above, but only if header names are relatively consistant throughout
+		 	 	first_variable=read[:1]
+		 		check_first_varable=False
+
+		 	if first_variable in read:  #While '@' is used as a quality score, '@M' has so far only been found in header
+		 	 	if read.split(" ",1)[0][1:] in vGoodReads: #checks to see if the header is in the good reads
+		 	 		vContinue=True
+					vGoodReads.remove(read.split(" ",1)[0][1:]) #if header is in good reads, it will be removed from good reads to save memory
+
+		 	if vContinue==True: #If this is turned on, the next four lines (which is the length of the sample), will be recorded
+		 	 	write_list.append(read)
+		 	 	x+=1
+			 	if x ==4:
+					x=0
+					vContinue=False
+
+			if len(vGoodReads)==0 and vContinue==False:
+				break
+	file.close()
+	fFileWrite('extOut_'+name+"_"+str(batch),write_list)
+	print("test")
 
 def fFileLength(file):
 	#Check that files are the same length
@@ -45,19 +84,23 @@ class extractApp():
 	def __init__(self):
 		self.verbose = False
 
-	def start(self, taxon, fixrank, fastq, threshold, output, batchsize):
+	def start(self, species, fixrank, fastq, threshold, output, batchsize, order ="", family="", genus="", procs=1):
 		starttime = time.time()
 
 		#Format input files:
-		print("---\nTaxon target is: " + taxon)
+		print("---\nSpecies target is: " + species)
 		vFixrankFile = open(fixrank, "r")
 		if fastq[-3:] == '.gz':
 			vFastqFile = gzip.open(fastq, "r")
 		else:
 			vFastqFile = open(fastq, "r")
 
-		#Run through the batches
+		loops_fixrank=0
+		loops_fastq=0
 
+		#if __name__ == '__main__'
+
+		#Run through the batches
 		print "Begining to break fixed rank into batches"
 		vFixBatchList = []
 		vBatch_fixrank,n = 0,0
@@ -77,7 +120,7 @@ class extractApp():
 						loops_fixrank+=1
 						n=0
 						print "Fix rank batch #"+str(loops_fixrank)+" written"
-				
+					
 				if len(vFixBatchList) >0:					
 					vBatch_fixrank =n
 					fFileWrite('extFixrankTemp_'+str(batchsize), vFixBatchList, False, str(loops_fixrank), True)
@@ -86,14 +129,8 @@ class extractApp():
 					continue_loop=False
 				else:
 					continue_loop=False
-
-
-		file.close()
-						
-
-
-		#Generate fastq batches
-
+			file.close()
+			#Generate fastq batches
 		n = 0
 		k = 0
 		j = 0
@@ -117,7 +154,7 @@ class extractApp():
 							j+=1
 							n+=1
 					if n == batchsize:	#If a batch size is reached, the batch will be written to file, the batch will be cleared, 
-										#and the counter for the number of loops for the fastq rank will increase
+											#and the counter for the number of loops for the fastq rank will increase
 						fFileWrite('extFastqTemp_'+str(batchsize), vTotal, True, str(loops_fastq), True)
 						n = 0
 						j = 0
@@ -136,19 +173,26 @@ class extractApp():
 					vList = []
 					vTotal = []
 					continue_loop=False
-		file.close()
+		file.close()	
+				
+
+
+
+
 		# Extract
 		vList = []
 		vFilePlace = 0		
 		vCounter = loops_fixrank
-		
+
 		print "Finding valid samples"
+		#global vGoodReads
 		vGoodReads=[]
 		while vCounter>=0: #Loops through all of the split fix rank files and records all good reads
 			with open('extFixrankTemp_'+str(batchsize)+"_"+str(vCounter)) as vFile:
 				for line in vFile.readlines():
-		 	 		if taxon in line and float(line.split("\t",26)[25]) >= float(threshold): #checks threshold and if the sample is above or at the score
-		 	 				vGoodReads.append(line.split("|",1)[0]) 		   #it will add the sample to the good reads file	
+					if (genus == "" or genus in line) and (family =="" or family in line) and (order =="" or order in line):
+			 	 		if species in line and float(line.split("\t",26)[25]) >= float(threshold): #checks threshold and if the sample is above or at the score
+			 	 				vGoodReads.append(line.split("|",1)[0]) 		   #it will add the sample to the good reads file	
 		 		vCounter-=1
 		 	vFile.close()
 		
@@ -159,36 +203,22 @@ class extractApp():
 		check_first_varable=True
 
 		print str(len(vGoodReads))+" valid samples found"
+
+
 		print "Extracting the samples. This may take a while"
 
+		vList=[]
+		temp=""
 		while vCounter>=0:	#Loops through all of the fastq files and writes all of the good reads
-		 	x=0
-		 	with open('extFastqTemp_'+str(batchsize)+"_"+str(vCounter), "r") as file:
-		 	 	for i, read in enumerate(file): #loops over the file. Increases i each step
-
-		 	 	######## @M may not be a good variable as it may change with speceis. save first two characters of line 1 and sort by that?
-					if check_first_varable==True: #this should fix problem above, but only if header names are relatively consistant throughout
-		 	 			first_variable=read[:1]
-		 	 			check_first_varable=False
-
-		 	 		if first_variable in read:  #While '@' is used as a quality score, '@M' has so far only been found in header
-		 	 			if read.split(" ",1)[0][1:] in vGoodReads: #checks to see if the header is in the good reads
-		 	 				vContinue=True
-							vGoodReads.remove(read.split(" ",1)[0][1:]) #if header is in good reads, it will be removed from good reads to save memory
-
-		 	 		if vContinue==True: #If this is turned on, the next four lines (which is the length of the sample), will be recorded
-		 	 			vList.append(read)
-		 	 			x+=1
-			 			if x ==4:
-							x=0
-							vContinue=False
-
-					if len(vGoodReads)==0 and vContinue==False:
-						break
-			file.close()
-			fFileWrite('extOut_'+taxon+"_"+str(batchsize),vList)
-			vList=[]
+		 	temp='extFastqTemp_'+str(batchsize)+"_"+str(vCounter)
+		 	vList.append(temp)
 			vCounter-=1
+		
+		pool=multiprocessing.Pool(procs)
+		func =partial(extractFastq, vGoodReads, batchsize, species)
+		pool.map(func, vList)
+		
+
 		print "Samples extracted"
 		print "Cleaning up"
 		fRemoveFiles(loops_fastq, loops_fixrank, batchsize)
